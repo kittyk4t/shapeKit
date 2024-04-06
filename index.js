9f45bc9339a518095ebce8dbb8e49bbbc6c23f27
kittyk4t21 <kmwa2020@mymail.pomona.edu>
2024-04-06 14:33:29 -0700

    background image from URL, transparent shape
    can have background image (from url) and transparent shape that can move over it

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ console.log("index.js");
 
 const canvas = new fabric.Canvas("canvas", {
     width: 500, height: 500,
-    backgroundColor: 'purple'
 });
+var imageURL = "https://s1.1zoom.me/big0/152/Foxes_Black_background_Tongue_Snout_Screaming_523460_1280x853.jpg";
+canvas.setBackgroundImage(imageURL, canvas.renderAll.bind(canvas), {
+    backgroundImageOpacity: 1,
+    originX: "left",
+    originY: "top",
+    left: 70,
+    scaleX: .5,
+    scaleY: .5,
+});
+
 canvas.add(new fabric.Rect({
     left: 100,
     top: 100,
-    fill: 'red',
+    stroke: 'red',
+    fill: 'rgba(0,0,0,0)',
     width: 20,
     height: 20,
     angle: 45
 }))
+
 canvas.renderAll();
