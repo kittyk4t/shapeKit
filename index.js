b852792a3ae63757576660670d8ee6e18da62faf
kittyk4t21 <kmwa2020@mymail.pomona.edu>
2024-04-08 15:16:34 -0700

    tidied some function; add shape buttons

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 console.log("index.js");
 
-const canvas = new fabric.Canvas("canvas", {
-    width: 500, height: 500,
-});
+
+const initCanvas = (id) => {
+    return new fabric.Canvas(id, {
+        width: 500, height: 500,
+    });
+}
+
+const setBackgroundImage = (url, canvas) => {
+    canvas.setBackgroundImage(url, canvas.renderAll.bind(canvas), {
+        backgroundImageOpacity: 1,
+        originX: "left",
+        originY: "top",
+        left: 70,
+        scaleX: .5,
+        scaleY: .5,
+    });
+}
 var imageURL = "https://s1.1zoom.me/big0/152/Foxes_Black_background_Tongue_Snout_Screaming_523460_1280x853.jpg";
-canvas.setBackgroundImage(imageURL, canvas.renderAll.bind(canvas), {
-    backgroundImageOpacity: 1,
-    originX: "left",
-    originY: "top",
-    left: 70,
-    scaleX: .5,
-    scaleY: .5,
-});
+const canvas = initCanvas("canvas");
 
-canvas.add(new fabric.Rect({
-    left: 100,
-    top: 100,
-    stroke: 'red',
-    fill: 'rgba(0,0,0,0)',
-    width: 20,
-    height: 20,
-    angle: 45
-}))
-
+setBackgroundImage(imageURL, canvas);
 canvas.renderAll();
+
+const createRect = (canvas) => {
+    const newRect = new fabric.Rect({
+        width: 100, height: 100, stroke: 'red',
+        fill: 'rgba(0,0,0,0)'
+    })
+    canvas.add(newRect)
+    canvas.renderAll()
+
+}
+
+const createCirc = (canvas) => {
+    console.log("circ")
+
+}
