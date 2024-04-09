4502ff0016541e597e9542ad12ac4f070777c16a
kittyk4t21 <kmwa2020@mymail.pomona.edu>
2024-04-08 17:57:09 -0700

    scales picture to canvas size
    stops from the weird cut offs fo the pictures

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ console.log("index.js");
 
 
 const initCanvas = (id) => {
+    console.log("this is called")
     return new fabric.Canvas(id, {
-        width: 500, height: 500, backgroundColor: "red"
+        width: 700, height: 500
     });
+
 }
 
 const setBackgroundImage = (url, canvas) => {
+
     canvas.setBackgroundImage(url, canvas.renderAll.bind(canvas), {
         backgroundImageOpacity: 1,
         originX: "left",
         originY: "top",
-        left: 0,
-        scaleX: .5,
-        scaleY: .5,
+        scaleX: canvas.width / url.width,
+        scaleY: canvas.height / url.height
     });
+
+    canvas.renderAll()
 }
 const createRect = (canvas) => {
     const newRect = new fabric.Rect({
@@ -39,7 +43,7 @@ const createCirc = (canvas) => {
 
 const createTri = (canvas) => {
     const newTri = new fabric.Triangle({
-        width: 100, height: 100, stroke: canvas.backgroundColor,
+        width: 100, height: 100, stroke: canvas.freeDrawingBrush.color,
         fill: 'rgba(0,0,0,0)'
     })
     canvas.add(newTri)
@@ -56,14 +60,6 @@ const clearCanvas = (canvas) => {
     canvas.renderAll()
 }
 
-
-// This code sets the background image
-// var imageURL = "https://s1.1zoom.me/big0/152/Foxes_Black_background_Tongue_Snout_Screaming_523460_1280x853.jpg";
-const canvas = initCanvas("canvas");
-
-// setBackgroundImage(imageURL, canvas);
-canvas.renderAll();
-
 // Add event listener to file input
 document.getElementById('file-input').addEventListener('change', function (e) {
     var file = e.target.files[0];
@@ -72,12 +68,13 @@ document.getElementById('file-input').addEventListener('change', function (e) {
         var img = new Image();
         img.onload = function () {
             var imgInstance = new fabric.Image(img, {
-                left: 100,
-                top: 100,
+                left: 0,
+                top: 0,
                 angle: 0,
                 opacity: 0.85
             });
             // canvas.add(imgInstance);
+            console.log("width " + imgInstance.width)
             setBackgroundImage(imgInstance, canvas);
         }
         img.src = event.target.result;
@@ -111,19 +108,18 @@ document.getElementById('file-input').addEventListener('change', function (e) {
 const setColorListener = () => {
     const picker = document.getElementById("colorPicker")
     picker.addEventListener("change", (event) => {
-        color = event.target.value
-        console.log(color)
-        canvas.backgroundColor = color
+        console.log(event.target.value)
+        newColor = "#" + event.target.value
+
+        canvas.freeDrawingBrush.color = color
         canvas.renderAll()
 
     })
 }
 // var imageURL = "https://s1.1zoom.me/big0/152/Foxes_Black_background_Tongue_Snout_Screaming_523460_1280x853.jpg";
-// const canvas = initCanvas("canvas");
+const canvas = initCanvas("canvas");
 
-
-setBackgroundImage(imageURL, canvas);
-canvas.renderAll();
-setColorListener();
+canvas.renderAll()
+setColorListener()
 
 
